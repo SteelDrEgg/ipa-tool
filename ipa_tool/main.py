e5e67afcc9902206c1f6855255007e9c2039d999
dregg <13710486486@163.com>
2022-08-17 09:35:56 -0400

    Fix bug of CLI can't write and print md5

--- ipa_tool/main.py
+++ ipa_tool/main.py
@@ -24,12 +24,14 @@ def get_info(ipa_path: str = typer.Argument(..., help="Aboslute path to .ipa fil
         general_task = progress.add_task("[green]Loading ipa", total=None)
         ipa_info = ipaInfos(ipa_path, get_multi_icon=multi_icon)
 
+    # Truning md5 to str in order to print or write
+    ipa_info.md5 = str(ipa_info.md5, encoding='ascii')
+    # If have output path write to json, or print it
     if len(output_path) > 0:
         output_path = str(pathlib.Path(output_path).absolute())
         for icon in ipa_info.icon.keys():
             open(output_path + '/' + icon, 'wb').write(ipa_info.icon[icon])
             ipa_info.icon[icon] = output_path + '/' + icon
-
         json.dump(ipa_info.__dict__,
                   open(output_path + '/ipa_infos.json', 'w'),
                   indent=2,
@@ -43,7 +45,6 @@ def get_info(ipa_path: str = typer.Argument(..., help="Aboslute path to .ipa fil
                        indent=2,
                        sort_keys=True)
         )
-
 
 @app.command()
 def cgbi2png(ipng_path: str = typer.Argument(..., help="Aboslute path to apple CgBI file"),
